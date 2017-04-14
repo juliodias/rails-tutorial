0c9ad9e9146331cb5d218b1ad70414a5af38df51
Julio Dias <julioocdias@gmail.com>
2017-04-14 20:59:32 -0300

    Adding some style to home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-    full_title = "#{page_title} | #{base_title}"
-    if page_title.empty? base_title else full_title
+    if page_title.empty?
+      base_title = "Ruby on Rails Tutorial Sample App"
+    else
+      full_title = "#{page_title} | #{base_title}"
+    end
   end
 end
