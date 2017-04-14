fba38df4232e450fb14f0a28e068158e6c270fa4
Julio Dias <julioocdias@gmail.com>
2017-04-14 14:21:02 -0300

    Some refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    full_title = "#{page_title} | #{base_title}"
+    if page_title.empty? base_title else full_title
+  end
 end
