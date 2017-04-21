c5f5a3c172332e76cdcd68abebd1f67b9c34c53c
Julio Dias <julioocdias@gmail.com>
2017-04-21 12:17:19 -0300

    Some tests and improving routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
-      base_title = "Ruby on Rails Tutorial Sample App"
+      base_title
     else
       full_title = "#{page_title} | #{base_title}"
     end
